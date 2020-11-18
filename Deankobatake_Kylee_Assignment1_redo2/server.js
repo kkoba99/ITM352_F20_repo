582e5d9d2c907a6cb34adc96b657343821e3499d
kkoba99 <kkak@hawaii.edu>
2020-11-18 01:08:51 -1000

    Final rework

--- Deankobatake_Kylee_Assignment1_redo2/server.js
+++ Deankobatake_Kylee_Assignment1_redo2/server.js
@@ -1,25 +1,26 @@
 //Assignment 1, Kylee Dean-Kobatake
 //Below is code extracted from Lab13, excercise 4 with slight modifications
-//Got help from Professor Port and Justina Bui from ITM352
+//Big thanks to Professor Port for the guidance he provided 
 
-//Below is code copied from Lab13, with additional help from Justina Bui in ITM352
-var data = require('./public/product_data.js'); // This gets the product data from the product_data.js file
-var products = data.products; // This puts the product data into an array called "products_array"
-const querystring = require('querystring');
+//Below is code copied from Lab13
+var data = require('./public/product_data.js'); // This puts the product_data.js data into a variable called data 
+var products = data.products; // This puts the product data into a variable called products
+const querystring = require('querystring'); //loads the querystring module in variable querystring
 var express = require('express'); // Starts express
-var app = express(); // Puts express in app
-var myParser = require("body-parser");
+var app = express(); // Puts express in variable app
+var myParser = require("body-parser"); //loads body-parser module in variable myParser
 
+// catches all incoming requests
 app.all('*', function (request, response, next) {
-    console.log(request.method + ' to ' + request.path);
-    next(); //go ahead to the next thing
+    console.log(request.method + ' to ' + request.path);//type in console the request method and request path
+    next(); //move on
 });
 
 app.use(myParser.urlencoded({ extended: true }));
 
-//Code below is referenced from Justina Bui in ITM352
+//Code below was made with guidance from Professor Port
 app.post("/process_quantities", function (request, response) {
-    let POST = request.body;
+    let POST = request.body;//put quantity_form POST data into variable called POST
     
     if (typeof POST['submitPurchase'] != 'undefined') {
         
@@ -42,17 +43,16 @@ app.post("/process_quantities", function (request, response) {
         if (hasqty == true && hasErrors == false) {
             response.redirect("./invoice.html?" + stringified);
         }
-        else { //if invalid, notify user and send them back to display
-            alertstr = `<script> alert("Error! Need to enter valid quantities");
+        else { //if no quantity inputted or invalid, notify user and send them back to display
+            alertstr = `<script> alert("Error! You need to select a item or enter valid quantities");
                         window.history.back() </script>`;
             
             response.send(alertstr);
          }
     } 
 });
-//Code above is referenced from Justina Bui in ITM352
 
-//Function below is from ITM352 Lab12, checks if quantities entered are a number,non-negative, and an integer
+//Function below is from ITM352 Lab12, checks if quantities entered are a number, non-negative, and an integer
 function isNonNegIntString(string_to_check, returnErrors = false) {
     errors = []; // assume no errors at first
     if (Number(string_to_check) != string_to_check) { errors.push('Not a number!'); } // Check if string is a number value
